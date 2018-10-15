ab6e20bfcc03166e951a990e11895cf11c1e1e0b
高垣 豊 <takagakiyutaka@takagaki-no-mba.local>
2018-10-16 01:39:20 +0900

    Initial Commit

---
+++ testRXswift1015/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testRXswift1015
+//
+//  Created by 高垣 豊 on 2018/10/16.
+//  Copyright © 2018年 yutaka takagaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
